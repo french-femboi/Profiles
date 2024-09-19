e53e0138bf3b6dfa5ee82982258d2ad7150ffc39
french-femboi <cat@french-femboi.eu>
2024-09-19 15:16:07 +0200

    updated projects page for the new style

---
+++ projects/index.php
@@ -0,0 +1,298 @@
+<!--
+                                                                                            
+  ,ad8888ba,                                                                                
+ d8"'    `"8b                ,d                                                             
+d8'                          88                                                             
+88             ,adPPYYba,  MM88MMM  8b,dPPYba,   ,adPPYYba,  8b      db      d8  888888888  
+88             ""     `Y8    88     88P'    "8a  ""     `Y8  `8b    d88b    d8'       a8P"  
+Y8,            ,adPPPPP88    88     88       d8  ,adPPPPP88   `8b  d8'`8b  d8'     ,d8P'    
+ Y8a.    .a8P  88,    ,88    88,    88b,   ,a8"  88,    ,88    `8bd8'  `8bd8'    ,d8"       
+  `"Y8888Y"'   `"8bbdP"Y8    "Y888  88`YbbdP"'   `"8bbdP"Y8      YP      YP      888888888  
+                                    88                                                      
+                                    88          
+
+⠀⠀⠀⠀⠀⠀⠀⠙⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⣿⡆⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⣾⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣷⡦⠀⠀⠀⠀⢰⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠃⣠⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀ MROWWWWWWWWWWWWWWWWWw
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣆⠀⠀⠀⣾⣿⣿⣿⣷⠄⠀⠰⠤⣀⠀⠀⣴⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠃⢺⣿⣿⣿⣿⡄⠀⠀⣿⣿⢿⣿⣿⣦⣦⣦⣶⣼⣭⣼⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣷⡆⠂⣿⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢙⣿⣿⣿⣿⣷⠸⣿⣿⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⡿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⢿⣿⣿⣿⣿⡄⣿⣿⣿⣿⣿⣿⡀⢀⣿⣿⣿⣿⠀⢸⣿⣿⠅⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡀⣠⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠠⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⡟⠋⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠙⢿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⠿⢿⡿⠛⠋⠁⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣅⠀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⡟⠃⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⣿⣿⣿⣿⣿⣤⡀⠀⠀⠀
+⠀⠜⢠⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣦⠄⣠⠀
+⠠⢸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠛⣿⣿⣿⡿⠏⠀⠀⠀⠀⠀⠀⢳⣾⣿⣿⣿⣿⣿⣿⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿
+⠀⢨⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⡿⡿⠿⠛⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠏⠉⠻⠿⠟⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              
+            CODED WITH LOVE AND A LOT OF TIME FROM CATPAWZ
+            
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Catpawz | PROJECTS</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&amp;display=swap" rel="stylesheet">
+  <link href="src/styles/output.css" rel="stylesheet">
+  <link rel="icon" href="favicon.png">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="description" content="I've coded on wayy to much stuff... here you can see everything...">
+  <meta name="author" content="Catpawz">
+  <meta content="#A443D1" data-react-helmet="true" name="theme-color">
+  <meta property="og:image" content="favicon.png">
+
+</head>
+
+<body class="bg-gray-900">
+
+  <div class="relative isolate overflow-hidden bg-gray-900">
+    <svg class="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
+      <defs>
+        <pattern id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <path d="M.5 200V.5H200" fill="none" />
+        </pattern>
+      </defs>
+      <svg x="50%" y="-1" class="overflow-visible fill-gray-800/20">
+        <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+      </svg>
+      <rect width="100%" height="100%" stroke-width="0" fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" />
+    </svg>
+    <div class="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]" aria-hidden="true">
+      <div class="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#A443D1] to-[#46dae5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"></div>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+      <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+        <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl flex items-center">
+          Catpawz's projects 
+          <img src="favicon.png" class="ml-4" style="height: 100px;">
+        </h1>
+        <p class="mt-6 text-lg leading-8 text-gray-300">I've coded on wayy to much stuff... here you can see everything...</p>
+        <div class="mt-10 flex items-center gap-x-6">
+          <a href="/" class="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm font-semibold text-purple-300 shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Home</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <h2 class="text-2xl font-bold text-white mb-6">Active projects</h2>
+    <h2 class="text-1xl font-bold text-gray-400 mb-6">Projects I'm actively working on, or maintaining</h2>
+    <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+      <table class="w-full border border-gray-700">
+        <thead>
+          <tr>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Name</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Description</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Type</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Links</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Information pages</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Pages to get information about me</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">Website</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              <a href="https://info.french-femboi.eu" target="_blank" class="text-purple-300 hover:text-purple-100">INFO</a> -
+              <a href="https://projects.french-femboi.eu" target="_blank" class="text-purple-300 hover:text-purple-100">PROJECTS</a> -
+              <a href="https://portfolio.french-femboi.eu" target="_blank" class="text-purple-300 hover:text-purple-100">PORTFOLIO</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Catappz</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Centralized location for all my apps</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">Website</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              <a href="https://apps.french-femboi.eu" target="_blank" class="text-purple-300 hover:text-purple-100">LINK</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Catkeys</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Centralized location for all my apps</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">App</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              <a href="https://play.google.com/store/apps/details?id=com.catpawz.catkeys" target="_blank" class="text-purple-300 hover:text-purple-100">PLAY STORE</a> -
+              <a href="https://github.com/french-femboi/Catkeys" target="_blank" class="text-purple-300 hover:text-purple-100">GITHUB</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Healing Neko</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Feeling a bit down, are we? No worries at all, fren!</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">App</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              <a href="https://github.com/french-femboi/healing-neko" target="_blank" class="text-purple-300 hover:text-purple-100">GITHUB</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Loving Neko</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">An app for any LDR</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">App</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              No links available
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">McMilford</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">A minecraft network owned by the great Blocklok</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-green-200 bg-green-900">Project</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              <a href="https://mcmilford.eu" target="_blank" class="text-purple-300 hover:text-purple-100">WEBSITE</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <h2 class="text-2xl font-bold text-white mb-6">Suspended projects</h2>
+    <h2 class="text-1xl font-bold text-gray-400 mb-6">Projects which I was working on, but stopped work for some time</h2>
+    <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+      <table class="w-full border border-gray-700">
+        <thead>
+          <tr>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Name</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Description</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Hypercore cloud</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Server hosting business</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-yellow-200 bg-yellow-900">Project</span>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Hypercore panel</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Server (KVM) management panel</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-yellow-200 bg-yellow-900">Project</span>
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Counting Neko</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Time tracking app</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-yellow-200 bg-yellow-900">Project</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <h2 class="text-2xl font-bold text-white mb-6">Discontinued projects</h2>
+    <h2 class="text-1xl font-bold text-gray-400 mb-6">Projects I actively worked on, which are now dead</h2>
+    <div class="bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+      <table class="w-full border border-gray-700">
+        <thead>
+          <tr>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Name</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Description</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Type</th>
+            <th class="py-3 px-4 bg-gray-900 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">Reason</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Dynapaw</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Small developer sharing platform</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-red-200 bg-red-900">Website</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              No relevance anymore
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Learn With Catpawz</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Learning platform for coding</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-red-200 bg-red-900">Website</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              Has to be restored as static pages
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Develoopers</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Snippet sharing platform</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-red-200 bg-red-900">Website</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              Couldn't keep up with server costs
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">NewDale network</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Minecraft network</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-red-200 bg-red-900">Project</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              Couldn't keep up with server costs
+            </td>
+          </tr>
+          <tr>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Yumi's story</td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">Unreal engine game about my life</td>
+            <td class="py-3 px-4 border-b border-gray-700">
+              <span class="inline-block py-1 px-2 rounded-full text-xs font-semibold text-red-200 bg-red-900">Project</span>
+            </td>
+            <td class="py-3 px-4 border-b border-gray-700 text-gray-300">
+              Data loss/ too brutal for my liking
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  </div>
+
+  <?php include 'inc/footer.php'; ?>
+</body>
+<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('french_femboi', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+</script>
+
+</html>
