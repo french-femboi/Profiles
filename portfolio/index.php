e88bb67cd3dca83f84537263f67a09324fa17be4
french-femboi <cat@french-femboi.eu>
2024-09-19 15:33:09 +0200

    updated style for the info page

---
+++ portfolio/index.php
@@ -0,0 +1,178 @@
+<!--
+                                                                                            
+  ,ad8888ba,                                                                                
+ d8"'    `"8b                ,d                                                             
+d8'                          88                                                             
+88             ,adPPYYba,  MM88MMM  8b,dPPYba,   ,adPPYYba,  8b      db      d8  888888888  
+88             ""     `Y8    88     88P'    "8a  ""     `Y8  `8b    d88b    d8'       a8P"  
+Y8,            ,adPPPPP88    88     88       d8  ,adPPPPP88   `8b  d8'`8b  d8'     ,d8P'    
+ Y8a.    .a8P  88,    ,88    88,    88b,   ,a8"  88,    ,88    `8bd8'  `8bd8'    ,d8"       
+  `"Y8888Y"'   `"8bbdP"Y8    "Y888  88`YbbdP"'   `"8bbdP"Y8      YP      YP      888888888  
+                                    88                                                      
+                                    88          
+
+⠀⠀⠀⠀⠀⠀⠀⠙⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⣿⡆⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⣾⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣷⡦⠀⠀⠀⠀⢰⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠃⣠⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀ MROWWWWWWWWWWWWWWWWWw
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣆⠀⠀⠀⣾⣿⣿⣿⣷⠄⠀⠰⠤⣀⠀⠀⣴⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠃⢺⣿⣿⣿⣿⡄⠀⠀⣿⣿⢿⣿⣿⣦⣦⣦⣶⣼⣭⣼⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣷⡆⠂⣿⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢙⣿⣿⣿⣿⣷⠸⣿⣿⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⡿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⢿⣿⣿⣿⣿⡄⣿⣿⣿⣿⣿⣿⡀⢀⣿⣿⣿⣿⠀⢸⣿⣿⠅⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡀⣠⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠠⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⡟⠋⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠙⢿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⠿⢿⡿⠛⠋⠁⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣅⠀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⣿⡟⠃⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⣿⣿⣿⣿⣿⣤⡀⠀⠀⠀
+⠀⠜⢠⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣦⠄⣠⠀
+⠠⢸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠛⣿⣿⣿⡿⠏⠀⠀⠀⠀⠀⠀⢳⣾⣿⣿⣿⣿⣿⣿⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿
+⠀⢨⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⡿⡿⠿⠛⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠏⠉⠻⠿⠟⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              
+            CODED WITH LOVE AND A LOT OF TIME FROM CATPAWZ
+            
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Catpawz | PORTFOLIO</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&amp;display=swap" rel="stylesheet">
+  <link href="src/styles/output.css" rel="stylesheet">
+  <link rel="icon" href="favicon.png">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="description" content="This is all the experience I have accummulated over the years!">
+  <meta name="author" content="Catpawz">
+  <meta content="#A443D1" data-react-helmet="true" name="theme-color">
+  <meta property="og:image" content="favicon.png">
+
+</head>
+
+<body class="bg-gray-900">
+
+  <div class="relative isolate overflow-hidden bg-gray-900">
+    <svg class="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
+      <defs>
+        <pattern id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <path d="M.5 200V.5H200" fill="none" />
+        </pattern>
+      </defs>
+      <svg x="50%" y="-1" class="overflow-visible fill-gray-800/20">
+        <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+      </svg>
+      <rect width="100%" height="100%" stroke-width="0" fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" />
+    </svg>
+    <div class="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]" aria-hidden="true">
+      <div class="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#A443D1] to-[#46dae5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"></div>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+      <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+        <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl flex items-center">
+          Catpawz's portfolio
+          <img src="favicon.png" class="ml-4" style="height: 100px;">
+        </h1>
+        <p class="mt-6 text-lg leading-8 text-gray-300">Welcome to my portfolio! On here you'll find everything about the experience I'v accummulated over the years, including work, hobbies, school and even more :")</p>
+        <div class="mt-10 flex items-center gap-x-6">
+          <a href="/" class="rounded-md bg-purple-900 px-3.5 py-2.5 text-sm font-semibold text-purple-300 shadow-sm hover:bg-purple-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-400">Home</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <h2 class="text-2xl font-bold text-white mb-6">Knowledge & Interests</h2>
+    <h2 class="text-1xl font-bold text-gray-400 mb-6">Here's some stuff you can check on, if you wanna ask me about stuff, or just things in general :")</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Code knowledge</h3>
+        <p class="text-gray-400">HTML, CSS, JS, PHP, Kotlin, Dart, GdScript, Java, C, C++, ...</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Graphic Design</h3>
+        <p class="text-gray-400">Creating visually appealing designs for various digital platforms.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Machine Learning</h3>
+        <p class="text-gray-400">Developing intelligent systems and models for data analysis.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Datacenters</h3>
+        <p class="text-gray-400">Building network and maintaining infrastructures.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Cloud Computing</h3>
+        <p class="text-gray-400">Leveraging cloud platforms for scalable and efficient solutions.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Data Science</h3>
+        <p class="text-gray-400">Analyzing and interpreting complex data to drive decision-making.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">DevOps</h3>
+        <p class="text-gray-400">Implementing practices to automate and improve software delivery.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-xl font-bold text-white mb-2">Artificial Intelligence</h3>
+        <p class="text-gray-400">Creating intelligent agents and systems that mimic human behavior.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <h2 class="text-2xl font-bold text-white mb-6">Education</h2>
+    <h2 class="text-1xl font-bold text-gray-400 mb-6">Here are some of the schools I've visited, keep in mind the german school system is a pain x"D. Course levels increase each year, so it will always increase every year.. until I stop :P</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-2xl font-bold text-white mb-2">PRIMARY SCHOOL</h3>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Stuttgart Sillenbuch 2013 - 2015</h4>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Course level 1-3</h4>
+        <p class="text-gray-400">Primary school is where children aged six to ten receive their foundational education in subjects like math, language, science, and social studies. It focuses on developing essential skills such as reading, writing, and basic arithmetic while promoting social growth.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-2xl font-bold text-white mb-2">PRIMARY SCHOOL</h3>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Stuttgart Ostfildern 2015 - 2017</h4>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Course level 3-5</h4>
+        <p class="text-gray-400">Primary school is where children aged six to ten receive their foundational education in subjects like math, language, science, and social studies. It focuses on developing essential skills such as reading, writing, and basic arithmetic while promoting social growth.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-2xl font-bold text-white mb-2">SECONDARY SCHOOL</h3>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Stuttgart Degerloch 2017 - 2023</h4>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Course level 5-10</h4>
+        <p class="text-gray-400">After completing secondary school, good students can attend a vocational high school or a high school. They can also attend a vocational school or do an apprenticeship.</p>
+      </div>
+      <div class="bg-gray-800 rounded-lg shadow-lg p-6">
+        <h3 class="text-2xl font-bold text-white mb-2">TECHNICAL HIGH SCHOOL</h3>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">stuttgart Vahingen 2023 - 2026</h4>
+        <h4 class="text-xl font-bold text-purple-200 mb-2">Course level 10-12</h4>
+        <p class="text-gray-400">The technical high school of this school offers young people interested in science / technology and correspondingly gifted the opportunity to acquire the general university entrance qualification (A-Levels).</p>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+
+  <?php include 'inc/footer.php'; ?>
+</body>
+<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('french_femboi', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+</script>
+
+</html>
